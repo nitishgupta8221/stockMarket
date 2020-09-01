f602284b16ac08e1c27348d5870072ca8d7c684d
nitishgupta8221 <68073038+nitishgupta8221@users.noreply.github.com>
2020-09-01 08:04:31 +0530

    Add files via upload

--- sector-service/src/main/java/project/nitish/SectorServiceApplication.java
+++ sector-service/src/main/java/project/nitish/SectorServiceApplication.java
@@ -3,6 +3,7 @@ package project.nitish;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SectorServiceApplication {
 
 	public static void main(String[] args) {
